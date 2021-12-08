7ee159eae89b959591a4eac526a5567b1e2d495f
manhkien198 <>
2021-12-08 21:31:27 +0700

    update

--- review/src/Review.js
+++ review/src/Review.js
@@ -1,23 +1,37 @@
 import React,{useState} from 'react';
 import people from './data'
+import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
 const Review=()=>{
     const [index,setIndex]= useState(0)
     const {id,name,job,image,text}= people[index]
     const checkNumber=(number)=>{
-        if(number>people.length+1){
+        if(number>people.length-1){
             return number = 0
         }
-        if(number)
+        if(number<0){
+            return number = people.length-1
+        }
+        return number
     }
     const nextPerson=()=>{
-
+        setIndex(index=>{
+            let newIndex=number+1
+            return checkNumber(newIndex)
+        })
     }
     const prevPerson=()=>{
-
+        setIndex(index=>{
+            let newIndex=number-1
+            return checkNumber(newIndex)
+        })
     }
     const randomPerson=()=>{
         const randomNumber = Math.floor(Math.random()*people.length)
         console.log(randomNumber);
+        if(randomNumber===index){
+            randomNumber=index+1
+        }
+        setIndex(checkNumber(randomNumber))
     }
 }
 export default Review
